ab0ad346ef50791deb3d2623cb71b5299b1e15cf
zacksmash <zachary.warren@me.com>
2025-10-25 23:11:27 -0600

    WIP 🚧

--- resources/mcp/stores/weather.ts
+++ resources/mcp/stores/weather.ts
@@ -1,13 +1,5 @@
-import { User } from '@mcp/types';
 import { defineStore } from 'pinia';
 
-export const useAppStore = defineStore('app', {
-    state: () => ({
-        user: null as User | null,
-    }),
-    actions: {
-        setUser(user: User) {
-            this.user = user;
-        },
-    },
+export const useWeatherStore = defineStore('weather', {
+    //
 });
